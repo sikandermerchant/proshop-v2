ac5e2cdcb17222cb8e4b7b76edcfad79f8693267
Sikander Merchant <sikander.merchant@mstratdigital.com>
2024-08-19 22:06:00 +0400

    Rating Component & Product Details Page

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+import Rating from './Rating'
 
 const Product = ({product}) => {
   return (
@@ -10,7 +11,7 @@ const Product = ({product}) => {
       </Link>
       <Card.Body>
         <Link to={`/product/${product._id}`}>
-          <Card.Title as='div'>
+          <Card.Title as='div' className='product-title'>
             <strong>{product.name}</strong>
           </Card.Title>
         </Link>
@@ -18,6 +19,9 @@ const Product = ({product}) => {
           <div className='my-3'>
             {product.rating} from {product.numReviews} reviews
           </div>
+        </Card.Text>
+        <Card.Text as='div'>
+          <Rating value={product.rating} text={`${product.numReviews} reviews`} />
         </Card.Text>
         <Card.Text as='h3'>
           ${product.price}
