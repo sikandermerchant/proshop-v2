68f2872885784e601a3490fa57a0ba95e102c7ed
Sikander Merchant <sikander.merchant@mstratdigital.com>
2024-10-08 21:33:26 +0400

    Adding Product Controller

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,25 +1,9 @@
 import express from "express";
 const router = express.Router();
-import asyncHandler from "../middleware/asyncHandler.js";
-import Product from '../models/productModel.js';
-// import products from '../data/products.js'; //data from local JS file
+import { getProducts,getProductByID } from "../controllers/productController.js";
 
-router.get('/', asyncHandler(async (req, res) => {
-  const products = await Product.find({});
-  res.json(products);
-}));
-router.get('/:id', asyncHandler(async (req, res) => {
-  const product = await Product.findById(req.params.id);
-  // const product = products.find((p) => p._id === req.params.id); handing array from local JS file
-  if(product){
-   return res.json(product);
-  }
-  // res.status(404).json({message: 'Product not found'});
-  else {
-    res.status(404);
-    throw new Error('Product not found');
-  }
-}));
+router.route("/").get(getProducts);
+router.route("/:id").get(getProductByID);
 
 
 export default router;
