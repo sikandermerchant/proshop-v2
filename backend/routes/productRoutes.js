90f9870879f2b82efa83bde4b2f668fe43305da2
Sikander Merchant <sikander.merchant@mstratdigital.com>
2024-10-08 21:18:21 +0400

    adding Middleware Error Handler

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -14,7 +14,11 @@ router.get('/:id', asyncHandler(async (req, res) => {
   if(product){
    return res.json(product);
   }
-  res.status(404).json({message: 'Product not found'});
+  // res.status(404).json({message: 'Product not found'});
+  else {
+    res.status(404);
+    throw new Error('Product not found');
+  }
 }));
 
 
